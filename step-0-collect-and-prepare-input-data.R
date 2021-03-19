2fc9f21ab7f8a3925f7376cffc0c7b7bbb402b64
Tim Riffe <tim.riffe@gmail.com>
2021-03-19 23:01:38 +0100

    starting redux of data prep

--- step-0-collect-and-prepare-input-data.R
+++ step-0-collect-and-prepare-input-data.R
@@ -1,16 +1,21 @@
-
-
+library(covidAgeData)
+library(here)
+library(DemoTools)
+library(readxl)
+library(tidyverse)
 #
 ## Set working directory:
 #
 
-setwd(./input-data)
+#setwd("./input-data")
 
 #
 ## 0. Some help functions
 #
 
 get_ungrouped_lx_100 <- function(current_lx_data){
+  # TR: how about spline(x=c(0,1,seq(5,100,5)),y=current_lx_data, method = "hyman")
+  #   for a monotonicity constraint?
 	smooth_current_lx_data <- smooth.spline(x=c(0,1,seq(5,100,5)),y=current_lx_data)
 	new_x <- c(seq(0,1,1),seq(2,5,1),seq(6,100,1))
 	predict_smooth_current_lx_data <- predict(smooth_current_lx_data,new_x,len=new_x)
@@ -50,29 +55,39 @@ lexp_age_specific <- function(lx,ax,mx){
 ## 1. Download and save data from COVer-AGE-DB
 #
 
-library(tidyverse)
-library(httr)
 
 download_data <- TRUE
 
-if(download_data==TRUE){	
+if (download_data){	
 
   ## Online source	
   url <- 'https://osf.io/7tnfh/download'
   
-  ## Create a folder *Data* 
-  if (!"COVer-AGE-DB" %in% dir()){
-    dir.create("COVer-AGE-DB")
+  cdb_dir <- here::here("input-data","COVerAGE-DB")
+  if (!dir.exists(cdb_dir)){
+    dir.create(cdb_dir)
   }
+  ## Create a folder *Data* 
+  # if (!"COVer-AGE-DB" %in% dir()){
+  #   dir.create("COVer-AGE-DB")
+  # }
+  
+  # TR: always downloads the most recent version. Fine until we submit.
+  # The file will be zipped. No need to unzip it in order to read it
+  # in however.
+  download_covid("Output_5",
+                 progress = TRUE, 
+                 dest = cdb_dir, 
+                 download_only = TRUE)
   
   ## Filename and where to save it on your computer
-  filename <- 'COVer-AGE-DB/Output_5.zip'
-  
-  ## Download the data
-  GET(url, write_disk(filename, overwrite = TRUE))
-  
-  ## Unzip the downloaded and saved zip file 
-  unzip(filename)
+  # filename <- 'COVer-AGE-DB/Output_5.zip'
+  # 
+  # ## Download the data
+  # GET(url, write_disk(filename, overwrite = TRUE))
+  # 
+  # ## Unzip the downloaded and saved zip file 
+  # unzip(filename)
 
 } ## if download_data
 
@@ -80,6 +95,34 @@ if(download_data==TRUE){
 #
 ## 2. Download and save data from UNWPP 2019
 #
+
+base_url <- "https://population.un.org/wpp/Download/Files/1_Indicators%20(Standard)/EXCEL_FILES/"
+
+file_names <- c("WPP2019_INT_F03_2_POPULATION_BY_AGE_ANNUAL_MALE.xlsx",
+                "WPP2019_INT_F03_3_POPULATION_BY_AGE_ANNUAL_FEMALE.xlsx",
+                "WPP2019_MORT_F17_1_ABRIDGED_LIFE_TABLE_BOTH_SEXES.xlsx",
+                "WPP2019_MORT_F17_2_ABRIDGED_LIFE_TABLE_MALE.xlsx",
+                "WPP2019_MORT_F17_3_ABRIDGED_LIFE_TABLE_FEMALE.xlsx",
+                "WPP2019_POP_F05_MEDIAN_AGE.xlsx")
+wpp_folders <- c("5_Interpolated/",
+                 "5_Interpolated/",
+                 "3_Mortality/",
+                 "3_Mortality/",
+                 "3_Mortality/",
+                 "1_Population/")
+
+#
+for (i in 1:6){
+  this_local_file <-  here::here("input-data",  file_names[i])
+  if (!file.exists(this_local_file)){
+    this.url  <- paste0(base_url,
+                        wpp_folders[i],
+                        file_names[i])
+    download.file(url = this.url,
+                  destfile = this_local_file)
+  }
+}
+
 
 #### Please go to UNWPP2019 website and download data files as described in "information-on-downloading-input-data.txt".
 
@@ -89,123 +132,184 @@ if(download_data==TRUE){
 
 ## 3.1 Load UNWPP2019 population counts in 2019:
 
-wom <- read.xlsx(file.path(the.data.path,paste("WPP2019_INT_F03_3_POPULATION_BY_AGE_ANNUAL_FEMALE.xlsx",sep="")),sheet = 1,startRow = 17)
-wom_select <- wom[which(wom[,"Reference.date.(as.of.1.July)"]=="2019"),c(3,8:109)] 
+wom <- read_excel(here::here("input-data","WPP2019_INT_F03_3_POPULATION_BY_AGE_ANNUAL_FEMALE.xlsx"),
+                  sheet = 1,
+                  skip = 16,
+                  na = "...") %>% 
+  dplyr::filter(`Reference date (as of 1 July)` == 2019) %>% 
+  dplyr::select(Location = `Region, subregion, country or area *`,
+                Year = `Reference date (as of 1 July)`,
+                `0`:`100`) %>% 
+  pivot_longer( `0`:`100`, names_to = "Age", values_to = "Population") %>% 
+  dplyr::filter(!is.na(Population)) 
 
-men <- read.xlsx(file.path(the.data.path,paste("WPP2019_INT_F03_2_POPULATION_BY_AGE_ANNUAL_MALE.xlsx",sep="")),sheet = 1,startRow = 17)
-men_select <- men[which(men[,"Reference.date.(as.of.1.July)"]=="2019"),c(3,8:109)] ## men[,c(3,8:109)]
+#
 
-## 3.2 Load UNWPP 2019 remaining life expectancy:
+men <- read_excel(here::here("input-data","WPP2019_INT_F03_2_POPULATION_BY_AGE_ANNUAL_MALE.xlsx"),
+                  sheet = 1,
+                  skip = 16,
+                  na = "...") %>% 
+  dplyr::filter(`Reference date (as of 1 July)` == 2019) %>% 
+  dplyr::select(Location = `Region, subregion, country or area *`,
+                Year = `Reference date (as of 1 July)`,
+                `0`:`100`) %>% 
+  pivot_longer( `0`:`100`, names_to = "Age", values_to = "Population") %>% 
+  filter(!is.na(Population)) 
 
-lt_1950_2020 <- read.xlsx("WPP2019_MORT_F17_1_ABRIDGED_LIFE_TABLE_BOTH_SEXES.xlsx",sheet = 1,startRow = 17)
 
-## 3.3 Vector with UN names for major countries used in metaregression of Levin et al. (2020): 
-
+## 3.2 Vector with UN names for major countries used in metaregression of Levin et al. (2020): 
 major_countries_Levin <- c("United Kingdom",
-					"Ireland",
-					"Italy",
-					"Netherlands",
-					"Portugal",
-					"Spain",
-					"Switzerland",
-					"United States of America",
-					"Belgium",
-					"France",
-					"Sweden",
-					"Australia",
-					"Iceland",
-					"Republic of Korea",
-					"Lithuania",
-					"New Zealand")
+                           "Ireland",
+                           "Italy",
+                           "Netherlands",
+                           "Portugal",
+                           "Spain",
+                           "Switzerland",
+                           "United States of America",
+                           "Belgium",
+                           "France",
+                           "Sweden",
+                           "Australia",
+                           "Iceland",
+                           "Republic of Korea",
+                           "Lithuania",
+                           "New Zealand")
+
+## 3.3 Load & filter UNWPP 2019 remaining life expectancy:
+lt_survivors <- read_excel(here::here("input-data","WPP2019_MORT_F17_1_ABRIDGED_LIFE_TABLE_BOTH_SEXES.xlsx"),
+           sheet = 1,
+           skip = 16,
+           na = "...") %>%
+ dplyr::select(Location = `Region, subregion, country or area *`,
+               Period,
+               Age = `Age (x)`,
+               nMx = `Central death rate m(x,n)`,
+               nAx = `Average number of years lived a(x,n)`,
+               lx = `Number of survivors l(x)`,
+               ex = `Expectation of life e(x)`) %>% 
+  dplyr::filter(Location %in% major_countries_Levin,
+         Period == "2015-2020")
+
+
 
 ## 3.4 Collect UN abridged life tables (number of survivors, a_x, m_x) for those countries:
 
-lt_survivors <- matrix(NA,nr=length(c(0,1,seq(5,100,5))),nc=length(major_countries_Levin))
-rownames(lt_survivors) <- c(0,1,seq(5,100,5))
-colnames(lt_survivors) <- major_countries_Levin
-
-for(country in 1:length(major_countries_Levin)){
-	coi <- major_countries_Levin[country]
-	current_lt <- as.numeric(lt_1950_2020[which(lt_1950_2020["Region,.subregion,.country.or.area.*"]==coi & lt_1950_2020["Period"]=="2015-2020"),"Number.of.survivors.l(x)"])
-	lt_survivors[,country] <- current_lt
-}
-
-lt_survivors 
-
-##
-##
-
-lt_ax <- matrix(NA,nr=length(c(0,1,seq(5,100,5))),nc=length(major_countries_Levin))
-rownames(lt_ax) <- c(0,1,seq(5,100,5))
-colnames(lt_ax) <- major_countries_Levin
-
-for(country in 1:length(major_countries_Levin)){
-	coi <- major_countries_Levin[country]
-	current_lt_ax <- as.numeric(lt_1950_2020[which(lt_1950_2020["Region,.subregion,.country.or.area.*"]==coi & lt_1950_2020["Period"]=="2015-2020"),"Average.number.of.years.lived.a(x,n)"])
-	lt_ax[,country] <- current_lt_ax
-}
-
-lt_ax
+# lt_survivors <- matrix(NA,nr=length(c(0,1,seq(5,100,5))),nc=length(major_countries_Levin))
+# rownames(lt_survivors) <- c(0,1,seq(5,100,5))
+# colnames(lt_survivors) <- major_countries_Levin
+# 
+# for(country in 1:length(major_countries_Levin)){
+# 	coi <- major_countries_Levin[country]
+# 	current_lt <- as.numeric(lt_1950_2020[which(lt_1950_2020["Region,.subregion,.country.or.area.*"]==coi & lt_1950_2020["Period"]=="2015-2020"),"Number.of.survivors.l(x)"])
+# 	lt_survivors[,country] <- current_lt
+# }
+# 
+# lt_survivors 
 
 ##
 ##
-
-lt_mx <- matrix(NA,nr=length(c(0,1,seq(5,100,5))),nc=length(major_countries_Levin))
-rownames(lt_mx) <- c(0,1,seq(5,100,5))
-colnames(lt_mx) <- major_countries_Levin
-
-for(country in 1:length(major_countries_Levin)){
-	coi <- major_countries_Levin[country]
-	current_lt_mx <- as.numeric(lt_1950_2020[which(lt_1950_2020["Region,.subregion,.country.or.area.*"]==coi & lt_1950_2020["Period"]=="2015-2020"),"Central.death.rate.m(x,n)"])
-	lt_mx[,country] <- current_lt_mx
-}
-
-lt_mx
-
-##
-##
-
-
-lt_ex <- matrix(NA,nr=length(c(0,1,seq(5,100,5))),nc=length(major_countries_Levin))
-rownames(lt_ex) <- c(0,1,seq(5,100,5))
-colnames(lt_ex) <- major_countries_Levin
-
-for(country in 1:length(major_countries_Levin)){
-	coi <- major_countries_Levin[country]
-	current_lt_ex <- as.numeric(lt_1950_2020[which(lt_1950_2020["Region,.subregion,.country.or.area.*"]==coi & lt_1950_2020["Period"]=="2015-2020"),"Expectation.of.life.e(x)"])
-	lt_ex[,country] <- current_lt_ex
-}
-
-lt_ex
+# 
+# lt_ax <- matrix(NA,nr=length(c(0,1,seq(5,100,5))),nc=length(major_countries_Levin))
+# rownames(lt_ax) <- c(0,1,seq(5,100,5))
+# colnames(lt_ax) <- major_countries_Levin
+# 
+# for(country in 1:length(major_countries_Levin)){
+# 	coi <- major_countries_Levin[country]
+# 	current_lt_ax <- as.numeric(lt_1950_2020[which(lt_1950_2020["Region,.subregion,.country.or.area.*"]==coi & lt_1950_2020["Period"]=="2015-2020"),"Average.number.of.years.lived.a(x,n)"])
+# 	lt_ax[,country] <- current_lt_ax
+# }
+# 
+# lt_ax
+# 
+# ##
+# ##
+# 
+# lt_mx <- matrix(NA,nr=length(c(0,1,seq(5,100,5))),nc=length(major_countries_Levin))
+# rownames(lt_mx) <- c(0,1,seq(5,100,5))
+# colnames(lt_mx) <- major_countries_Levin
+# 
+# for(country in 1:length(major_countries_Levin)){
+# 	coi <- major_countries_Levin[country]
+# 	current_lt_mx <- as.numeric(lt_1950_2020[which(lt_1950_2020["Region,.subregion,.country.or.area.*"]==coi & lt_1950_2020["Period"]=="2015-2020"),"Central.death.rate.m(x,n)"])
+# 	lt_mx[,country] <- current_lt_mx
+# }
+# 
+# lt_mx
+# 
+# ##
+# ##
+# 
+# 
+# lt_ex <- matrix(NA,nr=length(c(0,1,seq(5,100,5))),nc=length(major_countries_Levin))
+# rownames(lt_ex) <- c(0,1,seq(5,100,5))
+# colnames(lt_ex) <- major_countries_Levin
+# 
+# for(country in 1:length(major_countries_Levin)){
+# 	coi <- major_countries_Levin[country]
+# 	current_lt_ex <- as.numeric(lt_1950_2020[which(lt_1950_2020["Region,.subregion,.country.or.area.*"]==coi & lt_1950_2020["Period"]=="2015-2020"),"Expectation.of.life.e(x)"])
+# 	lt_ex[,country] <- current_lt_ex
+# }
+# 
+# lt_ex
 
 ## 3.5 Calculate life table assigning equal weight to major_countries_Levin:
 
-lt_survivors_single_age <- get_ungrouped_lx_100(current_lx_data=rowMeans(lt_survivors)) 
-ax <- c(rowMeans(lt_ax)[1],rep(0.5,length(seq(2,101,1))))
-lt_mx_single_age <- get_ungrouped_lx_100(current_lx_data=rowMeans(lt_mx)) 
-ex_major_countries_Levin <- lexp_age_specific(lx=as.vector(lt_survivors_single_age$y),ax=ax,mx=as.vector(lt_mx_single_age$y)) 
-names(ex_major_countries_Levin) <- 0:100
-ex_major_countries_Levin 
+agg_lt <-
+  lt_survivors %>% 
+  group_by(Age) %>% 
+  summarize(nMx = mean(nMx))
+
+
+# TR: alternatively, one could send in ndx as Deaths and nLx as exposures,
+# presumably arithmetic averaged. Result could be slightly different. This also differs
+# from results after taking means of nAx, lx, nMx 
+library(DemoTools)
+lt_unweighted <- lt_abridged2single(nMx = agg_lt$nMx, Age = agg_lt$Age)
+
+
+# lt_survivors_single_age  <- get_ungrouped_lx_100(current_lx_data=rowMeans(lt_survivors)) 
+# ax                       <- c(rowMeans(lt_ax)[1],rep(0.5,length(seq(2,101,1))))
+# lt_mx_single_age         <- get_ungrouped_lx_100(current_lx_data=rowMeans(lt_mx)) 
+# ex_major_countries_Levin <- lexp_age_specific(lx=as.vector(lt_survivors_single_age$y),ax=ax,mx=as.vector(lt_mx_single_age$y)) 
+# names(ex_major_countries_Levin) <- 0:100
+# ex_major_countries_Levin 
 
 ## 3.6 Collect UN population counts for those countries and calculate population weights:
 
-pop_count_l <- matrix(NA,nr=length(seq(0,100,1)),nc=length(major_countries_Levin))
-rownames(pop_count_l) <- seq(0,100,1)
-colnames(pop_count_l) <- major_countries_Levin
+pop_weights <-
+  wom %>% 
+  bind_rows(men) %>% 
+  dplyr::filter(Location %in% major_countries_Levin) %>% 
+  group_by(Location) %>% 
+  summarize(Population = sum(Population), .groups = "drop") %>% 
+  mutate(Prop = Population / sum(Population))
+  
+ 
+ 
+# pop_count_l <- matrix(NA,nr=length(seq(0,100,1)),nc=length(major_countries_Levin))
+# rownames(pop_count_l) <- seq(0,100,1)
+# colnames(pop_count_l) <- major_countries_Levin
+# 
+# for(country in 1:length(major_countries_Levin)){
+#   # TR: coi = country of interest?
+# 	coi <- major_countries_Levin[country]
+# 	current_pop_count <- as.numeric(wom_select[which(wom_select["Region,.subregion,.country.or.area.*"]==coi),3:103]) + 
+# 	  as.numeric(men_select[which(men_select["Region,.subregion,.country.or.area.*"]==coi),3:103])
+# 	pop_count_l[,country] <- current_pop_count
+# }
 
-for(country in 1:length(major_countries_Levin)){
-	coi <- major_countries_Levin[country]
-	current_pop_count <- as.numeric(wom_select[which(wom_select["Region,.subregion,.country.or.area.*"]==coi),3:103]) + as.numeric(men_select[which(men_select["Region,.subregion,.country.or.area.*"]==coi),3:103])
-	pop_count_l[,country] <- current_pop_count
-}
-
-pop_weights <- colSums(pop_count_l) / sum(colSums(pop_count_l))
+# pop_weights <- colSums(pop_count_l) / sum(colSums(pop_count_l))
 
 ## 3.7 Calculate life table assigning population weight to major_countries_Levin:
 
 #### 3.7.1 Ungroup population weighted average of lt_survivors into single years of age:
 
-lt_survivors_single_age_pop_weight <- get_ungrouped_lx_100(current_lx_data=rowSums(t(apply(X=lt_survivors,1,FUN=function(x){pop_weights*x})))) 
+lt_survivors_single_age_pop_weight <- 
+  get_ungrouped_lx_100(current_lx_data = rowSums(t(apply(X = lt_survivors, 1, FUN = function(x){pop_weights * x})))) 
+
+# TR: this one looks tricker to guess, but will try.
+
+
 
 #### 3.7.2 Determine ax for single years of age using population weighted average of major countries for age 0:
 
