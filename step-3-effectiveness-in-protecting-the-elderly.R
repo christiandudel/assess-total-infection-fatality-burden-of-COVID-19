f1729999d76c0ee4e139e04251343ff9aa8445c6
Christina Bohk-Ewald <christina.bohk@gmail.com>
2021-03-23 09:16:40 +0200

    add alternative measure for effectiveness in protecting the elderly

--- step-3-effectiveness-in-protecting-the-elderly.R
+++ step-3-effectiveness-in-protecting-the-elderly.R
@@ -408,6 +408,173 @@ dev.off()
 
 #
 ## 7. Calculate effectiveness in protecting the elderly (here called performance):
+## using total IFR(IFR_x,I_x) / total IFR (IFR_x,P_x)
 ## and perhaps account for infection prevalence
 #
+
+## dimnames(infections_array_Verity_scaled)
+
+## 7.1 Get population counts in 5 year age groups: 
+
+pop_count_5y <- apply(X=pop_count,2,FUN=function(x){
+						start_age <- seq(1,96,5)
+						stop_age <- seq(5,100,5)
+						output <- NA
+						for(age_group in 1:length(start_age)){
+							output[age_group] <- sum(x[start_age[age_group]:stop_age[age_group]])
+						}
+						return(output*1000)
+						})	
+rownames(pop_count_5y) <- seq(0,95,5)
+pop_count_5y						
+
+## 7.2 Estimate infection prevalence:
+
+
+  row_names <- c("total","60plus")
+  col_names <- dimnames(infections_array_Verity_scaled)[[1]]
+  matrix_names <- dimnames(infections_array_Verity_scaled)[[2]]
+
+  infection_prevalence_Verity_scaled <- array(NA,dim=c(length(row_names),length(col_names),length(matrix_names)),
+						dimnames=list(row_names,col_names,matrix_names))
+
+
+for(coi in 1:length(dimnames(infections_array_Verity_scaled)[[1]])){
+
+	current_coi <- dimnames(infections_array_Verity_scaled)[[1]][coi]
+	current_infections <- infections_array_Verity_scaled[current_coi,,,"central"]
+	current_pop_count_5y <- pop_count_5y[,current_coi]
+	current_infection_prevalence <- apply(current_infections,1,sum) / sum(current_pop_count_5y)
+	current_infection_prevalence_65plus <- apply(current_infections[,as.character(seq(65,95,5))],1,sum) / sum(current_pop_count_5y[as.character(seq(65,95,5))]) 
+	infection_prevalence_Verity_scaled[1,current_coi,] <- current_infection_prevalence*100 
+	infection_prevalence_Verity_scaled[2,current_coi,] <- current_infection_prevalence_65plus*100 
+}
+
+## 7.3 Re-calculate effectiveness in protecting the elderly (here called performance_v2)
+#### using total IFR(IFR_x,I_x) / total IFR (IFR_x,P_x): 
+
+performance_v2 <- (total_IFR_array_Verity_scaled[,,"central"] / initial_total_IFR_array_Verity_scaled[,"2019","central"])  
+## dimnames(performance_v2)
+
+## 7.4 Visualize effectiveness in protecting the elderly and collect core data:
+
+collect_latest_performance_v2 <- NA
+collect_ifr <- NA
+collect_latest_performance_v2_coi <- NA
+collect_latest_lambda <- NA
+collect_median_age_coi <- NA
+collect_latest_performance_v2_date <- NA
+
+require(wesanderson)
+pal <- c(wes_palette("Darjeeling1"),wes_palette("Darjeeling2"))
+names(pal)[1:length(unique(countries_by_world_region[,2]))] <- unique(countries_by_world_region[,2])
+
+setwd("./plots")
+
+dev.off()
+
+pdf(file="total-IFR-median-performance-v2-median-pop-age.pdf", width=10, height=10, family="Times", pointsize=16, onefile=TRUE)
+
+par(fig = c(0,1,0,1), las=1, mai=c(0.4,0.4,0.8,0.0))
+
+plot(-100,-100,xlim=c(-0.5,3.5),ylim=c(15,54),xlab="", ylab="", main="", axes=FALSE)
+title(bquote(atop("Effectiveness in protecting the elderly", "in relation to initial population risk and progress of the pandemic in each country")),font.main=2)
+
+points(x=seq(0,2.5,length=3),y=rep(55,3),pch=20,cex=1.8,col=pal[1:3])
+text(x=seq(0,2.5,length=3),y=rep(55,3),c(names(pal)[1:3]),font=2,pos=4,cex=0.7)
+points(x=seq(0,2.5,length=3),y=rep(53,3),pch=20,cex=1.8,col=pal[4:6])
+text(x=seq(0,2.5,length=3),y=rep(53,3),c(names(pal)[4:6]),font=2,pos=4,cex=0.7)
+
+segments(x0=seq(0,3.5,0.5),x1=seq(0,3.5,0.5),y0=15,y1=50,lty=2,col=gray(0.8))
+segments(x0=0,x1=3.5,y0=seq(20,50,5),y1=seq(20,50,5),lty=2,col=gray(0.8))
+
+lines(x=seq(-1,2.5,length=2),y=rep(0,2),col="black",lwd=3)
+lines(x=rep(1,2),y=seq(15,52,length=2),col="black",lwd=3)
+
+axis(side=1,at=seq(-0,3.5,0.25),labels=FALSE,lwd=1,pos=15)
+axis(side=1,at=seq(-0,3.5,0.5),labels=TRUE,lwd=3,pos=15)
+axis(side=2,at=seq(15,50,1),labels=FALSE,lwd=1,pos=-0.5)
+axis(side=2,at=seq(15,50,5),labels=TRUE,lwd=3,pos=-0.5)
+
+text(x=1,y=51,"More effective",font=2,pos=2)
+text(x=1,y=51,"Less effective",font=2,pos=4)
+
+text(x=-0.25,y=24,"Fewer elderly to protect",font=2,pos=3,srt=90) ## Lower initial risk
+text(x=-0.25,y=41,"More elderly to protect",font=2,pos=3,srt=90) ## Higher initial risk
+
+text(x=-0.65,y=51.75,"Median",font=2,cex=0.7,pos=4)
+text(x=-0.65,y=51,"population age",font=2,cex=0.7,pos=4)
+
+current_coi <- "Norway"
+
+for(coi in 1:length(all_countries)){
+	current_coi <- all_countries[coi]
+
+	current_ifr_ts <- total_IFR_array_Verity_scaled[current_coi,,"central"]
+	current_ifr <- rev(current_ifr_ts[which(!is.na(current_ifr_ts))])[1]
+	current_infection_prevalence_ts <- infection_prevalence_Verity_scaled[1,current_coi,]/100+1
+	current_infection_prevalence <- rev(current_infection_prevalence_ts[which(!is.na(current_infection_prevalence_ts))])[1] 
+	current_performance <- performance_v2[current_coi,]*current_infection_prevalence_ts
+	current_performance_latest <- rev(current_performance[which(!is.na(current_performance))])[1]
+
+	if(current_coi=="Norway"){
+		## dimnames(deaths_array)
+		## rowSums(deaths_array["Norway",,])
+		current_performance_latest <- current_performance["2021-01-24"]
+		current_ifr <- current_ifr_ts["2021-01-24"]
+		current_infection_prevalence <- current_infection_prevalence_ts["2021-01-24"]
+	}	
+
+	current_row <- which(countries_by_world_region[,1]==current_coi)
+	current_pal <- which(names(pal)==countries_by_world_region[current_row,2])
+
+	current_coi_long <- current_coi
+	if(current_coi == "USA"){
+		current_coi_long <- "United States of America"
+	}
+	if(current_coi == "Venezuela"){
+		current_coi_long <- "Venezuela (Bolivarian Republic of)"
+	}
+	if(current_coi == "Bolivia"){
+		current_coi_long <- "Bolivia (Plurinational State of)"
+	}
+	if(current_coi == "South Korea"){
+		current_coi_long <- "Republic of Korea"
+	}
+	if(current_coi == "Taiwan"){
+		current_coi_long <- "China, Taiwan Province of China"
+	}
+	if(current_coi == "Palestine"){
+		current_coi_long <- "State of Palestine"
+	}
+
+	current_median_age_pop <- as.numeric(median_age[which(median_age[,"Region,.subregion,.country.or.area.*"]==current_coi_long),"2020"])
+
+	if(length(current_median_age_pop)>0 & !is.na(current_performance_latest) & max(deaths_array[current_coi,,],na.rm=TRUE)>200){
+	
+		text(x=current_performance_latest, y=current_median_age_pop, current_coi, col=pal[current_pal], font=2)
+
+		collect_latest_performance_v2[coi] <- current_performance_latest
+		collect_ifr[coi] <- current_ifr
+		collect_latest_performance_v2_coi[coi] <- current_coi
+		collect_median_age_coi[coi] <- current_median_age_pop
+		collect_latest_performance_v2_date[coi] <- names(current_performance_latest)
+		collect_latest_lambda[coi] <- current_infection_prevalence
+
+	}
+
+}
+
+dev.off()
+ 
+## 7.5 Compare countries based on effectiveness in protecting the elderly:
+#### using subsets of countries that are perhaps defined based on threshold median age:   
+
+age_threshold <- 40
+
+collect_latest_performance_v2_coi[which(collect_median_age_coi>=age_threshold)][order(collect_latest_performance_v2[which(collect_median_age_coi>=age_threshold)],decreasing=FALSE)]
+collect_latest_performance_v2[which(collect_median_age_coi>=age_threshold)][order(collect_latest_performance_v2[which(collect_median_age_coi>=age_threshold)],decreasing=FALSE)]
+collect_latest_performance_v2_date[which(collect_median_age_coi>=age_threshold)][order(collect_latest_performance_v2[which(collect_median_age_coi>=age_threshold)],decreasing=FALSE)]
+## collect_median_age_coi[which(collect_median_age_coi>=age_threshold)][order(collect_latest_performance_v2[which(collect_median_age_coi>=age_threshold)],decreasing=FALSE)]
+
 
