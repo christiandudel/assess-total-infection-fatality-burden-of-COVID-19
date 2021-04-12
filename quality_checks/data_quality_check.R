6fe43f31138e0e08656363e284c8226e8e0fe071
Enrique Acosta <kikepaila@gmail.com>
2021-04-12 08:01:54 +0200

    quality chek update

--- quality_checks/data_quality_check.R
+++ quality_checks/data_quality_check.R
@@ -1,55 +1,59 @@
 library(osfr)
 library(tidyverse)
+require(lubridate)
+require(covidAgeData)
+require(here)
 
 # data_source <- "input-data/COVerAGE-DB/Output_5.zip"
 # link_old <- "https://osf.io/7tnfh/download?version=135&displayName=Output_5-2021-01-13T07%3A22%3A39.845954%2B00%3A00.zip"
-# osf_retrieve_file("7tnfh") %>%
-#   osf_download(path = "input-data/COVerAGE-DB/", conflicts = "overwrite")
+osf_retrieve_file("7tnfh") %>%
+  osf_download(path = "input-data/COVerAGE-DB/", conflicts = "overwrite")
 
-db_cov <-  read_csv("input-data/COVerAGE-DB/Output_5.zip",
+dat <-  read_csv("input-data/COVerAGE-DB/Output_5.zip",
                     skip = 3)
 
-db_cov2 <- 
-  db_cov %>% 
-  filter(Sex == "b",
-         Region == "All") %>% 
-  mutate(Date = dmy(Date)) %>% 
-  group_by(Country) %>% 
-  filter(Date == max(Date)) %>% 
-  ungroup()
+# db_cov2 <- 
+#   db_cov %>% 
+#   filter(Sex == "b",
+#          Region == "All") %>% 
+#   mutate(Date = dmy(Date)) %>% 
+#   group_by(Country) %>% 
+#   filter(Date == max(Date)) %>% 
+#   ungroup()
+# 
+# 
+# east <- c("Croatia", "Romania", "Malta", "Slovakia", "Poland", "Lithuania")
+# 
+# db_east <- 
+#   db_cov2 %>% 
+#   filter(Country %in% east) 
+# 
+# db_east %>% 
+#   group_by(Country, Date) %>% 
+#   summarise(Deaths = sum(Deaths))
 
 
-east <- c("Croatia", "Romania", "Malta", "Slovakia", "Poland", "Lithuania")
 
-db_east <- 
-  db_cov2 %>% 
-  filter(Country %in% east) 
-
-db_east %>% 
-  group_by(Country, Date) %>% 
-  summarise(Deaths = sum(Deaths))
+dat <- db_cov
 
 
-
-
-require(covidAgeData)
-require(here)
-require(lubridate)
-
-filename <- 
-  here::here("input-data","COVerAGE-DB","Output_5.zip")
-
-dat <- 
-  read_subset_covid(
-  filename, 
-  data = "Output_5", 
-  return = "tibble",
-  Region = "All",
-  Sex = "b") %>% 
-  mutate(Date = dmy(Date))
+# filename <- 
+#   here::here("input-data","COVerAGE-DB","Output_5.zip")
+# 
+# dat <- 
+#   read_subset_covid(
+#   filename, 
+#   data = "Output_5", 
+#   return = "tibble",
+#   Region = "All",
+#   Sex = "b") %>% 
+#   mutate(Date = dmy(Date))
 
 dat2 <- 
   dat %>% 
+  filter(Region == "All",
+         Sex == "b") %>% 
+  mutate(Date = dmy(Date)) %>% 
   group_by(Country, Date) %>% 
   summarise(Deaths = sum(Deaths),
             Cases = sum(Cases)) %>% 
@@ -98,7 +102,7 @@ for(i in 1:max(cts$gr)){
       axis.text.x = element_text(size = 5)
     )
   
-  ggsave(paste0("quality_checks/deaths_gr", i, ".png")) 
+  ggsave(paste0("quality_checks/deaths_210410_gr", i, ".png")) 
 }
 
 
