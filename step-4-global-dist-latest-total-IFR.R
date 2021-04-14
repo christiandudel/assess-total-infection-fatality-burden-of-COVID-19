4d24b1f4574c36175f132ac2320901d689b38ef0
Christina Bohk-Ewald <christina.bohk@gmail.com>
2021-04-14 21:45:06 +0300

    update results for latest IFR

--- step-4-global-dist-latest-total-IFR.R
+++ step-4-global-dist-latest-total-IFR.R
@@ -8,15 +8,15 @@ setwd(".")
 #
 ## 2. Zoom into the spatio-temporal distribution,
 #### and compare the total IFR at the latest COMMON date 
-#### (taking COVer-AGE-DB from 8 February 2021, this is 13 January 2021)
+#### (taking COVer-AGE-DB from 14 April 2021, this is 31 March 2021)
 #### based on COI-specific IFR_x and I_x/I 
 #### including sensitivity towards scaling IFR_x from various sources
 #
 
-## 2.1: Collect countries of interest with total IFR estimate as of 13 Jan 2021 and their population median age:
+## 2.1: Collect countries of interest with total IFR estimate as of 31 March 2021 and their population median age:
 
-## latest_common_date <- names(dates_and_number_of_values_lt_24)[length(dates_and_number_of_values_lt_24)]
-latest_common_date <- "2021-01-13"
+latest_common_date <- names(dates_and_number_of_values_lt_24)[length(dates_and_number_of_values_lt_24)]
+## latest_common_date <- "2021-03-31"
 countries_of_interest <- total_IFR_array_Verity_scaled[,as.character(latest_common_date),"central"]
 current_deaths <- apply(X=deaths_array[,latest_common_date,],1,function(x){sum(x)})
 countries_of_interest <- countries_of_interest[which(current_deaths>=200)] 
@@ -69,7 +69,7 @@ pdf(file=paste("total-IFR-latest-common-date-log-scale.pdf",sep=""), width=15, h
 par(fig = c(0,1,0,1), las=1, mai=c(0.4,0.4,1.0,0.4))
 
 plot(x=-100,y=-100,xlim=c(log(0.02),log(10+10)),ylim=c(22,52),xlab="",ylab="",main="",axes=FALSE)
-title(bquote(atop("Total infection fatality rate, in %", "Based on country-specific " ~ IFR[x] ~ " and " ~I[x] ~ " as of January 13, 2021")),font.main=2)
+title(bquote(atop("Total infection fatality rate, in %", "Based on country-specific " ~ IFR[x] ~ " and " ~I[x] ~ " as of March 31, 2021")),font.main=2)
 
 axis(side=1,at=log(c(0.05,0.1,0.25,0.5,1,2,3,5,10)),labels=c(0.05,0.1,0.25,0.5,1,2,3,5,10),lwd=3,pos=22)
 axis(side=4,at=seq(22,48,1),labels=FALSE,lwd=1,pos=log(10+5))
@@ -79,7 +79,8 @@ segments(x0=log(c(0.05,0.1,0.25,0.5,1,2,3,5,10)),x1=log(c(0.05,0.1,0.25,0.5,1,2,
 		y=18,y1=as.numeric(median_age[which(median_age[,"Region,.subregion,.country.or.area.*"]=="Portugal"),"2020"]),
 		col=gray(0.6),lwd=1,lty=2)
 
-text(x=log(10+5),y=48.5,"Median age",pos=3,font=2)
+text(x=log(10+5),y=49.25,"Population",pos=3,font=2)
+text(x=log(10+5),y=48.5,"median age",pos=3,font=2)
 
 points(x=seq(log(0.25),log(3),length=length(c("Verity","Levin","Salje"))),y=rep(51,length(c("Verity","Levin","Salje"))),pch=c(19,17,15,18),col="black",cex=2.5)
 
@@ -100,10 +101,15 @@ text(x=log(0.016),y=49.3,c(names(pal)[5]),font=2,pos=4,cex=0.6)
 points(x=log(0.016),y=48.3,pch=20,cex=1.8,col=pal[6])
 text(x=log(0.016),y=48.3,c(names(pal)[6]),font=2,pos=4,cex=0.6)
 
-selected_coi_for_vis_by_mean_age <- c("Portugal","South Korea",
-					"France","Sweden","Australia",
-						"Chile","Colombia","Mexico",
-						"Philippines","Jordan","Slovenia")
+selected_coi_for_vis_by_mean_age <- c("Italy","Germany","Slovenia",
+						"South Korea","France","Sweden","Australia",
+						"Colombia","Jamaica",
+						"Philippines","Jordan")
+
+## selected_coi_for_vis_by_mean_age <- c("Portugal","South Korea",
+##					"France","Sweden","Australia",
+##						"Chile","Colombia","Mexico",
+##						"Philippines","Jordan","Slovenia")
 
 for(country in 1:dim(coi_ifr_median_age)[1]){
 
